c19c6542258cf5275c520b54cbe28ac332eadbc6
cohaya1 <chika.ohaya@gmail.com>
2019-09-27 12:37:40 -0400

    Initial commit

---
+++ src/main/java/com/example/mycontactlistreform/ContactMapActivity.java
@@ -0,0 +1,17 @@
+package com.example.mycontactlistreform;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mycontactlistreform.R;
+
+public class ContactMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_map);
+    }
+}
